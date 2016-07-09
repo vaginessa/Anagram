bad3c78e465893321851331a8f39798c04429a26
Anthony Lackey <alackey96@gmail.com>
2016-07-09 13:35:38 -0700

    Finished app

--- app/src/main/java/com/google/engedu/anagrams/AnagramDictionary.java
+++ app/src/main/java/com/google/engedu/anagrams/AnagramDictionary.java
@@ -1,16 +1,16 @@
 package com.google.engedu.anagrams;
 
+import android.util.Log;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Random;
-import android.util.Log;
 
 public class AnagramDictionary {
 
@@ -18,17 +18,17 @@ public class AnagramDictionary {
     private static final int DEFAULT_WORD_LENGTH = 3;
     private static final int MAX_WORD_LENGTH = 7;
     private Random random = new Random();
+    private HashSet<String> wordSet = new HashSet<String>();
+    private ArrayList<String> wordList = new ArrayList<>();
+    private HashMap<String, ArrayList<String>> lettersToWord = new HashMap<String, ArrayList<String>>();
 
     public AnagramDictionary(InputStream wordListStream) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(wordListStream));
-        HashSet<String> wordSet = new HashSet<String>();
-        HashMap<String, ArrayList<String>> lettersToWord = new HashMap<String, ArrayList<String>>();
-
         String line;
-        ArrayList<String> wordList = new ArrayList<String>();
         while((line = in.readLine()) != null) {
             // Add word to list of all words
             String word = line.trim();
+            wordSet.add(word);
             wordList.add(word);
 
             // Add word to letterToWord
@@ -38,22 +38,25 @@ public class AnagramDictionary {
                 values.add(word);
                 lettersToWord.put(word_sorted, values);
 
-            // If sorted word already exists
-            } else {
-                lettersToWord.get(word_sorted).add(word);
-
             }
+            else lettersToWord.get(word_sorted).add(word);
         }
-        Log.d("Anthony", lettersToWord.get("opst").toString());
-
     }
 
     public boolean isGoodWord(String word, String base) {
+        Log.d("Answer","Word: " + word);
+        Log.d("Answer","Base: " + base);
+        Log.d("Answer","Result of contains (String): " + word.contains(base));
+        Log.d("Answer","Result of contains (HashSet): "+ wordSet.contains(word));
+        if (word.contains(base))
+            return false;
+        else if (!wordSet.contains(word))
+            return false;
         return true;
     }
 
     public ArrayList<String> getAnagrams(String targetWord) {
-        ArrayList<String> result = new ArrayList<String>();
+        ArrayList<String> result = lettersToWord.get(sortLetters(targetWord));
         return result;
     }
 
@@ -67,10 +70,24 @@ public class AnagramDictionary {
 
     public ArrayList<String> getAnagramsWithOneMoreLetter(String word) {
         ArrayList<String> result = new ArrayList<String>();
+        String sorted = sortLetters(word);
+        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        for (char c : alphabet){
+            ArrayList<String> temp = getAnagrams(sortLetters(sorted + c));
+            if (temp != null){
+                result.addAll(temp);
+            }
+        }
         return result;
     }
 
     public String pickGoodStarterWord() {
-        return "badge";
+        Random rand = new Random();
+        int i = rand.nextInt(9999+1);
+        while (lettersToWord.get(sortLetters(wordList.get(i))).size() < MIN_NUM_ANAGRAMS){
+            i = ++i%wordList.size();
+        }
+        Log.d("Answer", getAnagramsWithOneMoreLetter(wordList.get(i)).toString());
+        return wordList.get(i);
     }
 }
