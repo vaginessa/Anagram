baceef27d4507ba38a532198ee722357996bbef2
Anthony Lackey <alackey96@gmail.com>
2016-07-09 12:14:49 -0700

    Add missing sorted words to lettersToWords

--- app/src/main/java/com/google/engedu/anagrams/AnagramDictionary.java
+++ app/src/main/java/com/google/engedu/anagrams/AnagramDictionary.java
@@ -6,6 +6,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Random;
 import android.util.Log;
 
@@ -18,13 +20,26 @@ public class AnagramDictionary {
 
     public AnagramDictionary(InputStream wordListStream) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(wordListStream));
+        HashSet<String> wordSet = new HashSet<String>();
+        HashMap<String, ArrayList<String>> lettersToWord = new HashMap<String, ArrayList<String>>();
+
         String line;
         ArrayList<String> wordList = new ArrayList<String>();
         while((line = in.readLine()) != null) {
+            // Add word to list of all words
             String word = line.trim();
             wordList.add(word);
+
+            // Add word to letterToWord
+            String word_sorted = sortLetters(word);
+            if (lettersToWord.get(word_sorted) == null) {
+                ArrayList<String> values = new ArrayList<String>();
+                values.add(word);
+                lettersToWord.put(word_sorted, values);
+
+            }
         }
-        Log.d("Anthony", sortLetters(wordList.get(1)));
+        Log.d("Anthony", lettersToWord.get("opst").toString());
 
     }
 
@@ -34,14 +49,15 @@ public class AnagramDictionary {
 
     public ArrayList<String> getAnagrams(String targetWord) {
         ArrayList<String> result = new ArrayList<String>();
-
         return result;
     }
 
     private static String sortLetters(String word) {
+
         char[] word_array = word.toCharArray();
         Arrays.sort(word_array);
-        return new String(word_array);
+        String result = new String(word_array);
+        return result;
     }
 
     public ArrayList<String> getAnagramsWithOneMoreLetter(String word) {
